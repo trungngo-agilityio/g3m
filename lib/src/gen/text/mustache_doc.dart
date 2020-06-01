1565bb81aeec288172c676bcf34d51ad440b8e5b
Trung Ngo <trungngo@asnet.com.vn>
2020-06-01 15:33:52 +0700

    Adding mustache doc

---
+++ lib/src/gen/text/mustache_doc.dart
@@ -0,0 +1,19 @@
+part of g3gen;
+
+class MustacheDoc implements WritableProjectItem {
+  /// The internal compiled template.
+  final Template template;
+
+  // The values to be sent of rendering.
+  final dynamic values;
+
+  MustacheDoc(this.template, this.values);
+
+  factory MustacheDoc.of(String template, dynamic values) =>
+      MustacheDoc(Template(template), values);
+
+  @override
+  void write(StringSink out) {
+    template.render(values, out);
+  }
+}
