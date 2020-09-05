cfd0dfd03f47cf7503c9c8268e8114d04b69a7e3
Trung Ngo <trungngo@asnet.com.vn>
2020-09-05 21:53:58 +0700

    version 2 of model walker start working

--- lib/src/stimpack/model/model_walker.dart
+++ lib/src/stimpack/model/model_walker.dart
@@ -1,98 +1,163 @@
 part of g3.stimpack.model;
 
-typedef StimModelWalkerFunc = bool Function(
-    StimModelWalkContext context, dynamic node);
+typedef Func1<T1> = Function(StimModelWalkerMatch match, T1 e1);
+typedef Func2<T1, T2> = Function(StimModelWalkerMatch match, T1 e1, T2 e2);
+typedef Func3<T1, T2, T3> = Function(
+    StimModelWalkerMatch match, T1 e1, T2 e2, T3 e3);
 
-class StimModelWalkContext {
-  /// The internal list of processed symbols.
-  /// This set helps to never visit a symbol twice.
-  final Set _processed;
+class StimModelWalkerMatch {
+  /// This is the full path that leads to the result.
+  final List<StimSymbol> path;
 
-  final Queue<StimModelSymbol> _stack;
+  /// This is the match sequence.
+  final List<StimSymbol> match;
 
-  /// The stack of visited symbols.
-  /// This help handler to discover all contextual data
-  /// related to the currently processed symbol.
-  ///
-  List<StimModelSymbol> get stack => List.unmodifiable(_stack);
+  StimModelWalkerMatch._(this.path, this.match);
+}
 
-  T firstOfType<T>() {
-    return _stack.firstWhere(
-      (element) => element is T,
-      orElse: () => null,
-    ) as T;
-  }
+class _WalkJob {
+  final _WalkJob parent;
+  final dynamic item;
 
-  StimModelWalkContext._([Queue stack, Set processed])
-      : _processed = processed ?? {},
-        _stack = stack ?? Queue();
+  TypeMirror symbolMirror;
+  int matchCount = 0;
+  bool isSymbol = false;
+  bool matched = false;
+
+  _WalkJob(this.parent, this.item, this.matchCount);
 }
 
 class StimModelWalker {
-  final Map<StimModelType, StimModelWalkerFunc> _handler;
+  final StimModelSymbol symbol;
 
-  StimModelWalker([Map<StimModelType, StimModelWalkerFunc> handler])
-      : _handler = handler ?? {};
+  StimModelWalker(this.symbol);
 
-  StimModelWalker on({
-    @meta.required StimModelType type,
-    @meta.required StimModelWalkerFunc exec,
-  }) {
-    assert(type != null, 'on is required');
-    assert(exec != null, 'exec is required');
-    _handler[type] = exec;
-    return this;
+  void visit<T1>(Func1<T1> func) {
+    _doVisit(
+        query: [T1],
+        callback: (context) {
+          final m = context.match;
+          func.call(context, m[0] as T1);
+        });
   }
 
-  void walk({
-    @meta.required StimModelType type,
-    @meta.required dynamic instance,
-  }) {
-    assert(type != null, 'type is required');
-    final context = StimModelWalkContext._();
-    _doWalk(context, type, instance);
+  void visit2<T1, T2>(Func2<T1, T2> func) {
+    _doVisit(
+        query: [T1, T2],
+        callback: (context) {
+          final m = context.match;
+          func.call(context, m[0] as T1, m[1] as T2);
+        });
   }
 
-  void _doWalk(
-      StimModelWalkContext context, StimModelType type, dynamic symbol) {
+  void _doVisit({
+    List<Type> query,
+    void Function(StimModelWalkerMatch) callback,
+  }) {
+    // The original list of expected types, parent goes first.
+    final expectedTypes = query.map((e) => reflectType(e)).toList();
 
-    // Quits if the symbol has been visited.
-    if (symbol == null || context._processed.contains(symbol)) return;
+    _breadthFirstSearch((job) {
+      final s = job.item;
+      job.symbolMirror = reflect(s).type;
 
-    context._processed.add(symbol);
+      final expected = expectedTypes[job.matchCount];
+      if (job.symbolMirror.isAssignableTo(expected)) {
+        // another match discovered
+        job.matched = true;
+        job.matchCount++;
+        if (job.matchCount == expectedTypes.length) {
+          // A full match has been discovered
+          final result = _buildResult(job);
+          callback(result);
 
-    final r = reflect(symbol);
-
-    if (type.isCollection) {
-      // Special dart collection type that can be iterate through.
-      final iterable = symbol as Iterable;
-      for (final i in iterable) {
-        _doWalk(context, type.item, i);
-      }
-      return;
-    }
-
-    // Adds the symbol to the stack
-    context._stack.addFirst(symbol);
-
-    final func = _handler[type];
-    if (func != null) {
-      // Done walking
-      if (func.call(context, symbol) == true) return;
-    }
-
-    if (type.fields?.isNotEmpty == true) {
-      for (final f in type.fields) {
-        var fieldName = Symbol(f.name.camel().toString());
-        final fieldValue = r.getField(fieldName).reflectee;
-
-        if (fieldValue != null) {
-          _doWalk(context, f.type, fieldValue);
+          // Notifies that a match has been found and the breath first
+          // search should not goes any deeper.
+          return true;
         }
       }
+
+      // Notifies that a match has not been found and need to go deeper.
+      return false;
+    });
+  }
+
+  StimModelWalkerMatch _buildResult(_WalkJob job) {
+    // first need to collect the path.
+    var path = <StimSymbol>[];
+    var result = <StimSymbol>[];
+    while (job != null) {
+      if (job.isSymbol) {
+        final symbol = job.item;
+        path.add(symbol);
+        if (job.matched) result.add(symbol);
+      }
+      job = job.parent;
     }
 
-    // Removes the symbol from the stack
-    context._stack.removeFirst();
+    // Builds the final path.
+    path = path.reversed.toList();
+    result = result.reversed.toList();
+
+    return StimModelWalkerMatch._(path, result);
+  }
+
+  void _breadthFirstSearch(bool Function(_WalkJob job) handler) {
+    final processed = <dynamic>{};
+
+    final start = _WalkJob(null, symbol, 0);
+
+    final queue = Queue<_WalkJob>()..add(start);
+
+    while (queue.isNotEmpty) {
+      final job = queue.removeFirst();
+
+      // The current symbol has been visited before.
+      if (processed.contains(job.item)) continue;
+
+      processed.add(job.item);
+
+      if (job.item is Iterable) {
+        final iterable = job.item as Iterable;
+        // If the symbol is set, list, .... then just
+        // visit all of the items.
+        for (final i in iterable) {
+          queue.add(_WalkJob(job, i, job.matchCount));
+        }
+        continue;
+      }
+      
+      if (job.item is StimSymbol) {
+        job.isSymbol = true;
+      } else {
+        // Skips this item.
+        continue;
+      }
+      
+      // Stops go deeper if the handler returns true.
+      if (handler(job)) continue;
+
+      // Gets the mirror of the symbol.
+      final ms = reflect(job.item);
+
+      // Uses reflection to visit all child fields
+      final members = ms.type.instanceMembers.values;
+      for (final i in members) {
+        // Only visit public field.
+        if (!i.isGetter || i.isPrivate) continue;
+
+        // This is the field'job mirror
+        final mf = ms.getField(i.simpleName);
+
+        // This is the field'job value
+        final f = mf.reflectee;
+
+        // Ignore null value.
+        if (f == null) continue;
+
+        // If the field is a symbol then enqueue it for processing later.
+        queue.addLast(_WalkJob(job, f, job.matchCount));
+      }
+    }
   }
 }
--- test/stimpack/model/model_walker_test.dart
+++ test/stimpack/model/model_walker_test.dart
@@ -16,25 +16,14 @@ void main() {
         v = story.action;
 
     final s1 = s.of(actor: a.of(name: 'user'), action: v.signIn);
-    StimModelWalker()
-        .on(
-            type: t.story.story,
-            exec: (context, s) {
-              print('visit story: ${s.action.name}');
-              return false;
-            })
-        .on(
-            type: t.story.action,
-            exec: (context, s) {
-              print('visit action: ${s.name}');
-              return false;
-            })
-        .on(
-            type: t.story.actor,
-            exec: (_, actor) {
-              print(actor.name);
-              return false;
-            })
-        .walk(t.story.story, s1);
+
+//    StimModelWalker(s1).visit<StimStoryAction>((context, e1) {
+//      print('Found; ${e1.name}');
+//    });
+
+    StimModelWalker(s1).visit2<StimStoryStory, StimStoryAction>((context, e1, e2) {
+      print('Found; ${e1.name}, ${e2.name}');
+    });
+
   });
 }
