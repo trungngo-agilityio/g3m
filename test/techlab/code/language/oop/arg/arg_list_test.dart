674ec6806f50e933793e08f7dfd8f5a5f9cc9a82
Trung Ngo <trungngo@asnet.com.vn>
2021-01-30 17:48:18 +0700

    Arg list tests

--- test/techlab/code/language/oop/arg/arg_list_test.dart
+++ test/techlab/code/language/oop/arg/arg_list_test.dart
@@ -4,40 +4,79 @@ import 'package:test/test.dart';
 import '../utils.dart';
 
 void main() {
+  const empty = '';
   group('null', () {
     oopRunAndExpect(
       CodeArgList.of(required: null),
-      java: '',
+      dart: empty,
+      typescript: empty,
+      java: empty,
+      kotlin: empty,
     );
   });
 
   group('empty', () {
     oopRunAndExpect(
       CodeArgList.of(required: ''),
-      java: '',
+      dart: empty,
+      typescript: empty,
+      java: empty,
+      kotlin: empty,
     );
   });
 
   group('string', () {
+    const s1 = 'helloWorld';
     oopRunAndExpect(
       CodeArgList.of(required: 'hello world'),
-      java: 'helloWorld',
+      dart: s1,
+      typescript: s1,
+      java: s1,
+      kotlin: s1,
     );
   });
 
   group('node', () {
+    const s1 = 'helloWorld';
     oopRunAndExpect(
       CodeArgList.of(required: Text('hello world')),
-      java: 'helloWorld',
+      dart: s1,
+      typescript: s1,
+      java: s1,
+      kotlin: s1,
     );
   });
 
   group('name of another name', () {
-    var anotherName = CodeEnumValueName.of(name: 'hello world');
-    oopRunAndExpect(anotherName, java: 'HELLO_WORLD');
+    const s1 = 'helloWorld';
     oopRunAndExpect(
-      CodeArgList.of(required: anotherName),
-      java: 'helloWorld',
+      CodeArgList.of(
+        required: CodeEnumValueName.of(name: 'hello world'),
+      ),
+      dart: s1,
+      typescript: s1,
+      java: s1,
+      kotlin: s1,
+    );
+  });
+
+  group('long format', () {
+    const s1 = 'int i, String s, bool b';
+    const s2 = 'i: number, s: string, b: boolean';
+    const s3 = 'int i, String s, boolean b';
+    const s4 = 'i: Int, s: String, b: Boolean';
+    oopRunAndExpect(
+      CodeArgList.of(
+        required: [
+          CodeArg.of(name: 'i', type: CodeType.ofInteger()),
+          CodeArg.of(name: 's', type: CodeType.ofString()),
+          CodeArg.of(name: 'b', type: CodeType.ofBool()),
+        ],
+      ),
+      dart: s1,
+      typescript: s2,
+      java: s3,
+      kotlin: s4,
     );
   });
 }
--- test/techlab/code/language/oop/arg/arg_name_test.dart
+++ test/techlab/code/language/oop/arg/arg_name_test.dart
@@ -4,58 +4,49 @@ import 'package:test/test.dart';
 import '../utils.dart';
 
 void main() {
-  group('empty', () {
-    oopRunAndExpect(
-      CodeArgName.of(name: ''),
-      java: '',
-    );
-  });
+  const s1 = 'helloWorld';
 
   group('string', () {
     oopRunAndExpect(
       CodeArgName.of(name: 'hello world'),
-      java: 'helloWorld',
+      dart: s1,
+      typescript: s1,
+      java: s1,
+      kotlin: s1,
     );
   });
 
   group('node', () {
     oopRunAndExpect(
       CodeArgName.of(name: Text('hello world')),
-      java: 'helloWorld',
+      dart: s1,
+      typescript: s1,
+      java: s1,
+      kotlin: s1,
     );
   });
 
   group('name of another name', () {
     var anotherName = CodeEnumValueName.of(name: 'hello world');
-    oopRunAndExpect(anotherName, java: 'HELLO_WORLD');
     oopRunAndExpect(
       CodeArgName.of(name: anotherName),
-      java: 'helloWorld',
+      dart: s1,
+      typescript: s1,
+      java: s1,
+      kotlin: s1,
     );
   });
 
   group('with java private final', () {
     oopRunAndExpect(
       CodeArgName.of(
-        name: 'hello',
+        name: 'hello world',
         isPrivate: true,
       ),
-      java: 'hello',
-    );
-  });
-
-  group('with dart private final', () {
-    const s1 = '_hello';
-    const s2 = 'hello';
-    oopRunAndExpect(
-      CodeArgName.of(
-        name: 'hello',
-        isPrivate: true,
-      ),
-      dart: s1,
-      typescript: s2,
-      java: s2,
-      kotlin: s2,
+      dart: '_helloWorld',
+      typescript: s1,
+      java: s1,
+      kotlin: s1,
     );
   });
 }
